ab153cf9ee463606c9f6aea0bddbace3a9082452
Piotr <ratkowskipiotr04@gmail.com>
2026-03-10 21:26:37 +0100

    Initialize Client Class

---
+++ include/ChatClient.h
@@ -0,0 +1,36 @@
+#ifndef MULTI_THREADED_TCP_SERVER_CHATCLIENT_H
+#define MULTI_THREADED_TCP_SERVER_CHATCLIENT_H
+
+#include <string>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <atomic>
+
+class ChatClient {
+    int client_socket;
+    std::atomic<bool> is_connected;
+    std::atomic<bool> is_running;
+
+    //ImGui requires character array text
+    char ip_buffer[64];
+    char port_buffer[16];
+    char message_buffer[512];
+
+    std::vector<std::string> chat_history;
+    std::mutex chat_history_mutex;
+
+    std::thread receive_thread;
+
+    void receiveMessagee();
+
+    void addLog(const std::string& message);
+
+public:
+    ChatClient();
+    ~ChatClient();
+
+    void drawUI();
+};
+
+#endif //MULTI_THREADED_TCP_SERVER_CHATCLIENT_H
